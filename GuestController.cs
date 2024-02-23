005e37246a45c272c4f13f9fd242609db759a9ee
Titouan Auterioux <tauterioux@ensc.fr>
2024-02-23 15:16:22 +0100

    Update

--- GuestController.cs
+++ GuestController.cs
@@ -5,27 +5,34 @@ using MongoDB.Driver;
 
 namespace guest_manager_cli_system
 {
-    public class GuestModel
+    public class GuestController
     {
-        public GuestModel(){
+        public GuestController(){
 
+        }
+
+        public void Update(IMongoCollection<BsonDocument> collection, BsonDocument reference, BsonDocument modifier)
+        {
+            var documents = collection.FindOneAndReplace(reference,modifier);
         }
 
         public void Read(IMongoCollection<BsonDocument> collection)
         {
-            var documents = collection.Find(Builders<BsonDocument>.Filter.Empty).ToList();
+            var documents = collection.Find(new BsonDocument()).ToList();
             foreach (var document in documents)
             {
-                Console.WriteLine(document);
+                Console.WriteLine($"{document}");
             }
         }
+
+        
 
         public void Create(IMongoCollection<BsonDocument> collection, string firstName,string lastName, int age)
         {
             collection.InsertOne(CreateDocument(firstName,lastName,age));
         }
 
-        private BsonDocument CreateDocument(string firstName,string lastName, int age)
+        public BsonDocument CreateDocument(string firstName,string lastName, int age)
         {
             return new BsonDocument{
                 {"guest_id", new Random().Next(100000)},
@@ -33,7 +40,6 @@ namespace guest_manager_cli_system
                 {"first-name",firstName},
                 {"age",age}
             };
-
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -9,16 +9,17 @@ namespace guest_manager_cli_system
         static void Main(string[] args)
         {
 
-            DatabaseDebugger debugger = new DatabaseDebugger();
-            GuestModel Model = new GuestModel();
+            // DatabaseDebugger debugger = new DatabaseDebugger();
+            GuestController Controller = new GuestController();
 
             MongoClient Client = new MongoClient("mongodb://localhost:27017/");
             var Database = Client.GetDatabase("cs-api");
             
             var Collection = Database.GetCollection<BsonDocument>("users");
             
-            Model.Create(Collection,"Elena","Gilbert",17);
-            Model.Read(Collection);
+            BsonDocument modifier = Controller.CreateDocument("Jeremy","Gilbert",17);
+            Controller.Update(Collection,);
+            Controller.Read(Collection);
 
         }
 
